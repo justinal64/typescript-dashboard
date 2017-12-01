5ea3b478c3315cf38bebccf826fea4c63ed58e8f
justin leggett <justin@codingrookie.com>
2017-12-01 14:17:21 -0600

    created color.ts to store common colors used throughout the project

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+// Basic Colors
+const white = "white";
+
+// Navigation Colors
+const navigationLinkBG = "#4f666e";
+const navigationHoverBG = "#657f89";
+
+// Example from UI-Backpack
+// Link colors
+// const linkPrimary = "#297cbb";
+
+export default {
+  navigationLinkBG,
+  navigationHoverBG,
+  white
+};
