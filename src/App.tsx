3717495aa31f1849dded2a144c99fed9bdc50993
justin leggett <justin@codingrookie.com>
2017-12-01 10:32:15 -0600

    working on using flexbox to create the grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import { Col, Grid, Row } from "react-flexbox-grid";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navigation from "./components/navigation/index";
+// import Navigation from "./components/navigation/index";
 import Login from "./screens/Login";
 
 import "./styles/layout.css";
@@ -9,12 +10,26 @@ class App extends React.Component {
     return (
       <Router>
         <div>
-          {/* add navigation component here */}
-          <Navigation />
-
-          <Route exact path="/" component={Login} />
-          <Route path="/about" component={Login} />
-          <Route path="/topics" component={Login} />
+          <Grid>
+            <Row className="around-lg around-md around-sm">
+              <Col className="logo" xs={12} sm={12} md={2} lg={2}>
+                Logo Goes Here
+              </Col>
+              <Col className="header" xs={12} sm={12} md={10} lg={10}>
+                {/* <Navigation /> */}
+                Top Header
+              </Col>
+              <Col className="navigation" xs={12} sm={12} md={2} lg={2}>
+                Navigation
+              </Col>
+              <Col xs={12} sm={10} md={10} lg={10}>
+                {/* Make a routes component? */}
+                <Route exact path="/" component={Login} />
+                <Route path="/about" component={Login} />
+                <Route path="/topics" component={Login} />
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </Router>
     );
