37a2a35416efc5c0329c8406f0c1774c753a1f8c
justin leggett <justin@codingrookie.com>
2017-12-01 09:17:11 -0600

    added login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { BrowserRouter as Router, Link, Route } from "react-router-dom";
-import Hello from "./screens/Login";
+import Login from "./screens/Login";
 
 class App extends React.Component {
   public render() {
     return (
       <Router>
         <div>
+          {/* add navigation component here */}
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -21,7 +22,7 @@ class App extends React.Component {
 
           <hr />
 
-          <Route exact path="/" component={Hello} />
+          <Route exact path="/" component={Login} />
           {/* <Route path="/about" component={About} />
           <Route path="/topics" component={Topics} /> */}
         </div>
