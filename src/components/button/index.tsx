060b87d9240554eec46e6223308b3d235c49ef19
justin leggett <justin@codingrookie.com>
2017-12-14 11:36:42 -0600

    button is working correctly. I am slowly adding back props, so that one component will server multiple purposes.

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,39 +3,66 @@ import styled from "styled-components";
 import { getIcon } from "../../utils/helpers";
 
 const Button = styled.button`
-font-weight: 400;
-text-align: center;
-white-space: nowrap;
-vertical-align: middle;
-user-select: none;
-border: 1px solid transparent;
-padding: 0.5rem 0.75rem;
-line-height: 1.25;
-transition: all 0.15s ease-in-out;
-margin: 0.15em;
-background-color: transparent;
-font-size: ${(props: any) => (props.font ? props.font : ".75")}rem;
+  font-weight: 400;
+  text-align: center;
+  white-space: nowrap;
+  vertical-align: middle;
+  user-select: none;
+  border: 1px solid transparent;
+  padding: 0.5rem 0.75rem;
+  line-height: 1.25;
+  transition: all 0.15s ease-in-out;
+  margin: 0.15em;
+  background-color: transparent;
+  font-size: ${(props: any) => (props.font ? props.font : ".75")}rem;
+`;
 /* display: ${props => (props.display ? displayBlock : "inline-block")};
 ${props => themePicker(props.inverted, props.button)};
 ${props => (props.disabled ? disabledButton : "")};  */
-`;
 
-const StyledButton = (props: any) => {
-  const { icon, title, font, inverted, disabled, block, button } = props;
-  const needIcon = icon ? getIcon(icon) : "";
-  return (
-    <Button
-      button={button}
-      title={title}
-      font={font}
-      inverted={inverted}
-      disabled={disabled}
-      display={block}
-    >
-      {needIcon}
-      {title}
-    </Button>
-  );
-};
+export interface Props {
+  title: string;
+  icon?: string;
+  button?: string;
+  // font?: float;
+}
+
+class StyledButton extends React.Component<Props, object> {
+  public render() {
+    const { title, icon, button } = this.props;
+    const needIcon = icon ? getIcon(icon) : "";
+    return (
+      <Button
+        button={button}
+        title={title}
+        // font={font}
+        // inverted={inverted}
+        // disabled={disabled}
+        // display={block}
+      >
+        {needIcon}
+        {title}
+      </Button>
+    );
+  }
+}
+
+// const StyledButton = (props: any) => {
+//   const { icon, title, font, inverted, disabled, block, button } = props;
+//   const needIcon = icon ? getIcon(icon) : "";
+//   return (
+//     <Button
+//       button={button}
+//       title={title}
+//       font={font}
+//       inverted={inverted}
+//       disabled={disabled}
+//       display={block}
+//     >
+//       {needIcon}
+//       {title}
+//     </Button>
+//   );
+// };
 
 export default StyledButton;
--- src/containers/Button.tsx
+++ src/containers/Button.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import StyledButton from "../components/button/index";
 
 export interface Props {
   name?: string;
@@ -7,20 +8,12 @@ export interface Props {
 
 class Button extends React.Component<Props, object> {
   public render() {
-    // const { name, enthusiasmLevel = 1 } = this.props;
-
-    // if (enthusiasmLevel <= 0) {
-    //   throw new Error("You could be a little more enthusiastic. :D");
-    // }
-
-    return <div>Welcome to the button page</div>;
+    return (
+      <div>
+        <StyledButton title="test" icon={"puzzle-piece"} />
+      </div>
+    );
   }
 }
 
 export default Button;
-
-// helpers
-
-// function getExclamationMarks(numChars: number) {
-//   return Array(numChars + 1).join("!");
-// }
